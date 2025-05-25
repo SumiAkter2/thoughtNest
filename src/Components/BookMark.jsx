8331be28bbfbb76a5b5141df7878ffb61f7c8318
SumiAkter2 <akteralisha36@gmail.com>
2025-05-25 12:59:32 +0600

    show bookmark title

--- src/Components/BookMark.jsx
+++ src/Components/BookMark.jsx
@@ -1,7 +1,9 @@
-import React from "react";
-
-const BookMark = () => {
-  return <div></div>;
+const BookMark = ({ bookmarkItem }) => {
+  return (
+    <div className="mt-4 bg-[#8b3b003b] p-2 rounded-md">
+      <h1 className="my-4 text-lg font-bold">{bookmarkItem.title}</h1>
+    </div>
+  );
 };
 
 export default BookMark;
--- src/Components/BookMarks.jsx
+++ src/Components/BookMarks.jsx
@@ -1,7 +1,14 @@
+import BookMark from "./BookMark";
+
 const BookMarks = ({ bookmark }) => {
   return (
-    <div className="md:w-1/3">
-      <h1>Book Mark added: {bookmark.length}</h1>
+    <div className="md:w-1/3 bg-gray-100 p-4">
+      <h1 className="text-3xl font-bold text-[#8b3b00]">
+        Bookmarked Blogs: {bookmark.length}
+      </h1>
+      {bookmark.map((bookmarkItem) => (
+        <BookMark key={bookmarkItem.id} bookmarkItem={bookmarkItem}></BookMark>
+      ))}
     </div>
   );
 };
